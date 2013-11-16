52b01afcf4f7ee71c426e15871b2f7ed6487c727
Clark Zhang <blastpower2@gmail.com>
2013-11-16 00:27:13 -0500

    made a crapton of fragments. note: its pretty late but not really im gonna go watch the season finale of korra now

---
+++ src/com/ranintotree/ride/fragments/FragmentRoutes.java
@@ -0,0 +1,25 @@
+package com.ranintotree.ride.fragments;
+
+import com.ranintotree.ride.R;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class FragmentRoutes extends ListFragment {
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		String[] values = getResources().getStringArray(R.array.routes_array);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+	}
+}
---
+++ src/com/ranintotree/ride/fragments/FragmentStops.java
@@ -0,0 +1,7 @@
+package com.ranintotree.ride.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class FragmentStops extends Fragment {
+
+}
